950f7cedeed9ae4c13897cd07c5d40ee33ae6eca
Rodrigo Tapia <roytapia-ferrari@hotmail.com>
2015-09-07 09:31:17 -0500

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+int F,C;
+cout << "put the temperature in Fahrenheit" << endl;
+cin >> F;
+C=5*(F-32)/9;
+cout << "A temperature of " << F << " degrees Fahrenheit is " << C <<  " in Celsius " << endl;
+if (C >= 70)
+  {
+    cout << "The water will boil at this conditions" << endl;
+  }
+  else
+  {
+    cout << "the Water does not boil at this temperature" << endl;
+  }
+return 0;
+}
